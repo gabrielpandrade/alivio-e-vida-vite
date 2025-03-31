17143cd0ac7dbb3df52180d90c8fd05b75c861c3
Gabriel Pinto Andrade <pintoandradegabriel@gmail.com>
2025-03-31 02:16:56 -0300

    add: carousels

--- src/components/sections/Ambiente.jsx
+++ src/components/sections/Ambiente.jsx
@@ -1,4 +1,6 @@
 import Section from "../Section";
+import { useState, useRef } from "react";
+import { useEffect } from "react";
 
 const Ambiente = () => {
   return (
@@ -8,52 +10,28 @@ const Ambiente = () => {
       </h2>
       <div className="flex flex-col text-secondary-content max-w-[80dvw] sm:max-w-xl md:max-w-2xl lg:max-w-5xl mx-auto">
         <div className="flex flex-col md:flex-row items-center">
-          <img
-            className="w-[400px] max-h-[200px] object-cover rounded-lg"
-            src="/lazer.jpg"
-            alt="Lazer"
-            width={600}
-            height={400}
-          />
+          <RotativeCarousel imgPaths={['/carousel/1/img_1.jpg', '/carousel/1/img_2.jpg', '/carousel/1/img_3.jpg']} />
           <p className="text-center p-8">
             <span className="font-bold">Lazer e conforto:</span> Espaços de
             convivência, piscina, salão de TV com canais de séries e filmes.
           </p>
         </div>
         <div className="flex flex-col md:flex-row-reverse items-center">
-          <img
-            className="w-[400px] max-h-[200px] object-cover rounded-lg"
-            src="/comida.jpg"
-            alt="Lazer"
-            width={600}
-            height={400}
-          />
+        <RotativeCarousel imgPaths={['/carousel/2/img_1.jpg', '/carousel/2/img_2.jpg', '/carousel/2/img_3.jpg']} />
           <p className="text-center p-8">
             <span className="font-bold">Alimentação Balanceada:</span> Refeições
             nutritivas para manter a saúde e o equilíbrio dos pacientes.
           </p>
         </div>
         <div className="flex flex-col md:flex-row items-center">
-          <img
-            className="w-[400px] max-h-[200px] object-cover rounded-lg"
-            src="/quartos.jpg"
-            alt="Lazer"
-            width={600}
-            height={400}
-          />
+        <RotativeCarousel imgPaths={['/carousel/3/img_1.jpg', '/carousel/3/img_2.jpg']} />
           <p className="text-center p-8">
             <span className="font-bold">Quartos Confortáveis:</span> Opções
             coletivas, privativas e Prime, com ar-condicionado e hidromassagem.
           </p>
         </div>
         <div className="flex flex-col md:flex-row-reverse items-center">
-          <img
-            className="w-[400px] max-h-[200px] object-cover rounded-lg"
-            src="/monitoramento.jpg"
-            alt="Lazer"
-            width={600}
-            height={400}
-          />
+        <RotativeCarousel imgPaths={['/carousel/4/img_1.jpg', '/carousel/4/img_2.jpg']} />
           <p className="text-center p-8">
             <span className="font-bold">Monitoramento 24h:</span> Câmeras e
             equipe sempre disponível e suporte contínuo para qualquer
@@ -62,6 +40,36 @@ const Ambiente = () => {
         </div>
       </div>
     </Section>
+  );
+};
+
+const RotativeCarousel = ({ imgPaths }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const intervalRef = useRef(null);
+
+  const totalImages = imgPaths.length;
+
+  useEffect(() => {
+    const nextImage = () => {
+      setCurrentIndex((prev) => (prev + 1) % totalImages);
+    };
+
+    intervalRef.current = setInterval(nextImage, 3000);
+
+    return () => clearInterval(intervalRef.current);
+  }, [totalImages]);
+
+  return (
+    <div className="w-[300px] h-[300px] max-w-[300px] max-h-[300px] min-w-[300px] min-h-[300px]">
+      <img
+        src={imgPaths[currentIndex]}
+        alt={`Imagem ${currentIndex + 1}`}
+        width={300}
+        height={300}
+        className="w-full h-full object-cover rounded-lg transition-opacity duration-500"
+      />
+    </div>
   );
 };
 
